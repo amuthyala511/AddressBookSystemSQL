7cccdc30722100bd9bc77eb92cdb44745c2e619b
Aishwarya <amuthyala511@gmail.com>
2020-11-20 00:38:06 +0530

    Added: Code to insert new records into AddressBook

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -10,3 +10,8 @@ Zip INT(30) NOT NULL,
 PhoneNumber INT(30) NOT NULL,
 Email VARCHAR(50) NOT NULL );
 SHOW TABLES;
+INSERT INTO contacts ( FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email ) VALUES
+( 'Aish', 'Reddy', 'PeaceVilla', 'KNR', 'TG', 505001, 9087654321, 'aish@gmail.com' ),
+( 'Panu', 'Muthyala', 'SCHomes', 'HYD', 'TG', 505468, 9876543210, 'panu@gmail.com' ),
+( 'Shiv', 'Kumar', 'MCHS', 'Bangalore', 'KA', 560076, 7689054321, 'shiv@yahoo.com' ),
+( 'Vishal', 'Chander', 'Perambur', 'Chennai', 'TN', 600011, 7098654321, 'chan@gmail.com' );
